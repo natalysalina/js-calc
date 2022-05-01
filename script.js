f313b4e88395ba29b7fe0280256ff3a9ecc6b042
natalysalina <88324033+natalysalina@users.noreply.github.com>
2022-05-01 22:28:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function culcPlus() {
+    let a = prompt("Напиши первое число", "");
+    let b = prompt("Напиши второе число", "");
+    alert(a+b);
+}
+function culcMinus() {
+    let a = prompt("Напиши первое число", "");
+    let b = prompt("Напиши второе число", "");
+    alert(a-b);
+}
+function culcDeleniye() {
+    let a = prompt("Напиши первое число", "");
+    let b = prompt("Напиши второе число", "");
+    alert(a/b);
+}
+function culcUmnozh() {
+    let a = prompt("Напиши первое число", "");
+    let b = prompt("Напиши второе число", "");
+    alert(a*b);
+}
